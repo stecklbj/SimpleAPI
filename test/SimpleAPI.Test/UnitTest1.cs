9b90bb666a50ffd409da20960980b76542177b51
Brenda J Steckler <Brenda.J.Steckler@ey.com>
2020-04-20 15:07:44 -0700

    Initial Commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    ValuesController controller = new ValuesController();[Fact]
+    public void GetReturnsCorrectNumber()
+    {
+        var returnValue = controller.Get(1);
+        Assert.Equal("Brenda Steckler", returnValue.Value);
+    }
+    }
+}
